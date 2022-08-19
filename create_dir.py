1fc6de33f127d9185e5f83c4191cf22f9e9fc8e6
MrUmair1 <mrumair95@gmail.com>
2022-08-19 15:21:13 +0500

    create dir in python

---
+++ create_dir.py
@@ -0,0 +1,19 @@
+from genericpath import exists
+import os 
+    
+# Leaf directory 
+
+
+dir_names = ["list_tables"]
+for i in dir_names:
+# Parent Directories 
+    try:
+        parent_dir = "SAP_SOURCE/"
+            
+        # Path 
+        path = os.path.join(parent_dir, i) 
+        
+ 
+        os.makedirs(path) 
+    except:
+        print ("already exist")
